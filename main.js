6b825f6021fc6e290e879b100616bc49e3896837
Thiago Cortez <thiago.when@hotmail.com>
2023-08-31 18:27:43 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('form').submit(function(e){
+        e.preventDefault()
+
+        const nomeDaTarefa = $('#item').val()
+        const novaLinha = $('<li></li>')
+
+        $(`<p>${nomeDaTarefa}</p>`).appendTo(novaLinha)
+        $('#list').append(novaLinha)
+
+        $('#item').val('')
+    })
+
+    $('ol').on('click','li', function(){
+        $(this).addClass('riscado')
+    })
+})
